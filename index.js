398a9e7c64c8f7f8198769e5c283b99cf833341a
Avani Rawat <avanirawat30@gmail.com>
2026-06-19 11:42:15 +0530

    fix render port configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
