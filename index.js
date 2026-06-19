2eba51b34479ce0b251b23dd0fd39da0a8da2925
Avani Rawat <avanirawat30@gmail.com>
2026-06-19 11:27:02 +0530

    Configure dynamic port for deployment

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
+    app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => {
     console.log(err);
